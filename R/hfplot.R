feb872a9fadeb3cb0d53ad8b374b9fb243caffde
Peter Solymos <psolymos@gmail.com>
2016-02-10 15:05:22 -0700

    initial upload

---
+++ R/hfplot.R
@@ -0,0 +1,28 @@
+hfplot <- function (
+    region = c("Alberta", 
+        "Canadian Shield", 
+        "Boreal", 
+        "Foothills", 
+        "Rocky Mountain", 
+        "Parkland", 
+        "Grassland"),
+    type = c("Total", 
+        "Human-created Water Bodies", 
+        "Agriculture", 
+        "Urban, Rural & Industrial", 
+        "Mines, Wells & Other Energy Features", 
+        "Vegetated Linear Industrial Features", 
+        "Transportation", 
+        "Forest Harvest")) {
+    region <- match.arg(region)
+    type <- match.arg(type)
+    Year <- c(1999, 2001, 2002:2014)
+    Footprint <- hf[region,type,]
+    Rate <- diff(range(Footprint)) / diff(range(Year))
+    p <- ggplot2::qplot(Year, Footprint, 
+        ylab=paste(type, "Footprint (%)"), 
+        main=paste0(region, " (", round(Rate,2), "%/yr)"),
+        geom=c("point","line")) + theme_grey(base_size = 18)
+	print(p)  
+	invisible()
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+OpenCPU App: Footprint Change
+---------------------
+
+Simple OpenCPU Application. To install in R:
+
+    library(devtools)
+    install_github("psolymos/footprintchange")
+
+    library(opencpu)
+    opencpu$browse("library/footprintchange/www")
+
+
+For more information about OpenCPU apps, see [opencpu.js](https://github.com/jeroenooms/opencpu.js#readme)
